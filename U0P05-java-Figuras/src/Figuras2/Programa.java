4d50c6830c2259bdfbedc53ac1296599cc42fb4c
piternoob <pedroplazar94@gmail.com>
2017-10-02 11:58:07 +0200

    toci

--- U0P05-java-Figuras/src/Figuras2/Programa.java
+++ U0P05-java-Figuras/src/Figuras2/Programa.java
@@ -21,12 +21,12 @@ public class Programa {
 		double aTotal, aCir1, aCir2, aCua1, aTri1;
 		double pi=Math.PI;
 		
-		aCir1=(pi*Math.pow(cir1.getRadio(), 2))/2;
-		aCir2=(pi*Math.pow(cir2.getRadio(), 2))*0.75;
+		aCir1=pi*Math.pow(cir1.getRadio(), 2);
+		aCir2=pi*Math.pow(cir2.getRadio(), 2);
 		aCua1=Math.pow(cua1.getLado(), 2);
-		aTri1=(tri1.getBase()*tri1.getAltura())/2;
+		aTri1=tri1.getBase()*tri1.getAltura()/2;
 		
-		aTotal=aCir1+aCir2+aCua1+aTri1;
+		aTotal=aCir1/2+aCir2+0.75+aCua1+aTri1;
 		
 		System.out.println("El area de la figura es: "+aTotal+".");
 	}
@@ -35,12 +35,12 @@ public class Programa {
 		double pTotal, pCir1, pCir2, pCua1, pTri1;
 		double pi=Math.PI;
 		
-		pCir1=pi*cir1.getRadio();
-		pCir2=1.5*pi*cir2.getRadio();
-		pCua1=cua1.getLado()*3;
-		pTri1=tri1.getBase()+tri1.getAltura()+Math.sqrt(Math.pow(tri1.getBase(), 2)+Math.pow(tri1.getAltura(), 2))-2*(cir1.getRadio()+cir2.getRadio())-cua1.getLado();
+		pCir1=2*pi*cir1.getRadio();
+		pCir2=2*pi*cir2.getRadio();
+		pCua1=cua1.getLado()*4;
+		pTri1=tri1.getBase()+tri1.getAltura()+Math.sqrt(Math.pow(tri1.getBase(), 2)+Math.pow(tri1.getAltura(), 2));
 	
-		pTotal=pCir1+pCir2+pCua1+pTri1;
+		pTotal=pCir1/2+pCir2*0.75+pCua1-cua1.getLado()+pTri1-(cir1.getRadio()*2-cir2.getRadio()*2-cua1.getLado());
 		
 		System.out.println("El perimetro de la figura es: "+pTotal+".");
 	}
