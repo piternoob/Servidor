e7b64ce601f9b909adecd0865858dfd839ad7290
piternoob <pedroplazar94@gmail.com>
2017-10-17 14:23:55 +0200

    hh

--- UltimaPrueba/index.php
+++ UltimaPrueba/index.php
@@ -1,3 +1,11 @@
 <?php
-echo "<h1>Funciona/h1>"
+$nombre="Pedro";
+$apellido="Plaza";
+echo "<h1>Funciona</h1>\n";
+echo "<h3>Comillas dobles: Mi nombre $nombre y mi apellido $apellido</h3>\n";
+echo '<h2>Comillas simples: Mi nombre $nombre y mi apellido $apellido</h2>';
+define("NOMBRE", "Pedro Plaza");
+echo "<p>Mi nombre es ".Nombre."</p>";
+define("NOMBRE", "Pedro Plaza", true);
+echo "<p>Mi nombre es ".Nombre."</p>";
 ?>
