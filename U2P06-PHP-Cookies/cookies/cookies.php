4e912895a3d411482dfb9b191ef2faddbfe33ddd
piternoob <pedroplazar94@gmail.com>
2017-11-08 12:38:03 +0100

    .

--- U2P06-PHP-Cookies/cookies/cookies.php
+++ U2P06-PHP-Cookies/cookies/cookies.php
@@ -1,3 +1,9 @@
+<?php 
+if (isset($_REQUEST["borrar"])){
+    setcookie("visitante", $_POST["nombre"], time() - 15, "/");
+    header("Location: ".$_SERVER["PHP_SELF"]);
+  }
+?>
 <html>
 <head>
 <title>Cookies</title>
@@ -6,7 +12,7 @@
 <body>
 <?php
 if(isset($_POST["enviar"])) {
-    setcookie("visitante", $_POST["nombre"], time() + (86400 * 15), "/"); // 86400 = segundos en 1 día
+    setcookie("visitante", $_POST["nombre"], time() + 150, "/"); // 86400 = segundos en 1 día  
 }
 if(isset($_COOKIE["visitante"])) {
     echo "<h2>Damos la bienvenida a $_COOKIE[visitante]</h2>";
@@ -23,4 +29,5 @@ else
 }
 ?>
 <p><a href="<?php echo $_SERVER['PHP_SELF']?>">Enlace a esta misma página</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']?>?borrar=true">Eliminar cookie</a></p>
 </body></html>
