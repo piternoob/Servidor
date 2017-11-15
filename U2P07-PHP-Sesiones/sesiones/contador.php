5da1879bb480991b4b042e55749c8192e50fa420
piternoob <pedroplazar94@gmail.com>
2017-11-15 13:04:59 +0100

    .

--- U2P07-PHP-Sesiones/sesiones/contador.php
+++ U2P07-PHP-Sesiones/sesiones/contador.php
@@ -1,12 +1,32 @@
 <?php
-if (session_status() == PHP_SESSION_NONE)
+if (session_status() == PHP_SESSION_NONE) {
+    session_name('idSesion00');
     session_start();
-if (isset($_SESSION['contador'])) 
-    $_SESSION['contador'] += 1;
-else
-    $_SESSION['contador'] = 0;
-$mensaje = "Has visitado esta página " . $_SESSION['contador'] . " veces en esta sesión.";
+}
+if (isset($_REQUEST["reiniciarContador"]))
+    unset($_SESSION["contador"]);
+if (isset($_REQUEST["cerrarSesion"])) {
+    $_SESSION = array();
+    session_unset();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    }
+    session_destroy();
+}
+if (session_status() == PHP_SESSION_NONE)
+    $mensaje = "No hay sesión iniciada";
+else {
+    if (isset($_SESSION['contador']))
+        $_SESSION['contador'] += 1;
+    else
+        $_SESSION['contador'] = 1;
+    if(isset($_POST["enviar"]) && !empty($_POST["valor"]))
+        $_SESSION['contador'] =$_POST["valor"];
+    $mensaje = "Has visitado esta página " . $_SESSION['contador'] . " veces en esta sesión.";
+}
 ?>
+
 <html>
 <head>
 <title>Sesiones</title>
@@ -14,5 +34,11 @@ $mensaje = "Has visitado esta página " . $_SESSION['contador'] . " veces en est
 </head>
 <body>
 <h3><?php echo $mensaje;?></h3>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"], ENT_QUOTES, "UTF-8");?>" method="post">
+Valor del contador: <input type="number" name=valor>
+<input type="submit" name="enviar" value="Enviar Datos">
+</form>
 <p><a href="<?php echo $_SERVER['PHP_SELF']?>">Recargar la página</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?reiniciarContador=true"?>">Reiniciar contador</a></p>
+<p><a href="<?php echo $_SERVER['PHP_SELF']."?cerrarSesion=true"?>">Cerrar sesión</a></p>
 </body></html>
