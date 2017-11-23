740183ff4d3ce408f3c149582cb45b9bb7d5f8be
piternoob <pedroplazar94@gmail.com>
2017-11-23 14:25:28 +0100

    .

---
+++ U3P02-Acceso a BBDD en PHP/conexiones/conexion1.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Conexión a BBDD con PHP</title>
+    <meta charset="UTF-8"/>
+</head>
+<body>
+<h2>Pruebas con la base de datos de animales</h2>
+<?php
+$servidor = "localhost";
+$usuario = "alumno";
+$clave = "alumno";
+?>
+<!-- PRUEBAS: -->
+<p> Vamos a utilizar las siguientes variables:</p>
+<ul>
+<?php
+echo "<li>Nombre del servidor al que nos vamos a conectar a MySQL: $servidor</li>";
+echo "<li>Nombre de usuario con el que nos vamos a conectar a MySQL: $usuario</li>";
+echo "<li>Contraseña del usuario en MySQL: $clave</li>";
+?>
+</ul>
+<?php
+echo "<h3>Estableciendo conexión...</h3>";
+$conexion = new mysqli($servidor,$usuario,$clave);
+if ($conexion->connect_errno) {
+    echo "<p>Error al establecer la conexión (" . $conexion->connect_errno . ") " . $conexion->connect_error . "</p>";
+}
+else {
+    echo "<p>Información de servidor: $conexion->host_info</p>";
+    echo "<h3>Desconectando...</h3>";
+    mysqli_close($conexion);
+}
+?>
+</body>
+</html>
---
+++ U3P02-Acceso a BBDD en PHP/conexiones/conexion2.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<title>Conexión a BBDD con PHP</title>
+<meta charset="UTF-8"/>
+</head>
+<body>
+<h2>Pruebas con la base de datos de animales</h2>
+<?php
+$servidor = "localhost";
+$usuario = "alumno";
+$clave = "alumno";
+?>
+<!-- PRUEBAS: -->
+<?php
+$conexion = new mysqli($servidor,$usuario,$clave,"animales");
+//si quisiéramos hacerlo en dos pasos:
+// $conexion = new mysqli($servidor,$usuario,$clave);
+// $conexion->select_db("animales");
+$conexion->query("SET NAMES 'UTF8'");
+
+if ($conexion->connect_errno) {
+    echo "<p>Error al establecer la conexión (" . $conexion->connect_errno . ") " . $conexion->connect_error . "</p>";
+}
+echo "<p>A continuación mostramos algunos registros:</p>";
+echo "<p>Animales:</p>";
+$resultado = $conexion -> query("select * FROM Animal ORDER BY Nombre");
+if($resultado->num_rows === 0) 
+    echo "<p>No hay animales en la base de datos</p>";
+$fila=$resultado->fetch_assoc();
+while($fila!=null) {
+    echo "<hr>";
+    echo "Nombre:" . $fila['nombre'];
+    echo "<br>Especie: $fila[especie]"; // observa la diferencia en el uso de comillas
+    $fila=$resultado->fetch_assoc();
+}
+mysqli_free_result($resultado);
+echo "<p>Cuidadores:</p>";
+$resultado = $conexion -> query("select * FROM cuidador ORDER BY Nombre");
+if($resultado->num_rows === 0)
+    echo "<p>No hay cuidadores en la base de datos</p>";
+    $fila=$resultado->fetch_assoc();
+    while($fila!=null) {
+        echo "<hr>";
+        echo "Nombre:" . $fila['Nombre'];
+        $fila=$resultado->fetch_assoc();
+    }
+echo "<h3>Desconectando...</h3>";
+mysqli_close($conexion);
+?>
+</body>
+</html>
