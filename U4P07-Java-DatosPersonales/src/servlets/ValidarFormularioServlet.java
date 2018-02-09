8152629a4f7f26a4c566e89160e276ebba366540
piternoob <pedroplazar94@gmail.com>
2018-02-09 11:48:45 +0100

    .

--- U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
+++ U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
@@ -2,10 +2,6 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ProcesarDatosServlet
+ * Servlet implementation class ValidarFormularioServlet
  */
-@WebServlet("/ProcesarDatos")
-public class ProcesarDatosServlet extends HttpServlet {
+@WebServlet("/ValidarFormulario")
+public class ValidarFormularioServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ProcesarDatosServlet() {
+    public ValidarFormularioServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -33,20 +29,32 @@ public class ProcesarDatosServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html;charset=UTF-8");
+		
+		String nombre, contraseña, mayusculas;
+		
         PrintWriter out = response.getWriter();
-        
-        out.println("<html><head><meta charset='UTF-8'/><title>Procesar datos</title></head>");
-        out.println("<body><h1>Datos del formulario</h1>");
-        out.println("<h3>Nombre: Valor</h3>");
        
-        out.println("<ul>");
-        out.println("<li>Nombre: "+request.getParameter("text")+"</li>");
-        
-        out.println("<li>Contraseña: "+request.getParameter("pass")+"</li>");
-        
-        out.println("<li>Numero: "+request.getParameter("number")+"</li>");
+        if(request.getParameter("text")!=null)
+        	nombre=request.getParameter("text");
+        else
+        	nombre="El campo está vacío.";
         	
+        out.println("<li>Contraseña: "+request.getParameter("pass")+"</li>");
+        if(request.getParameter("pass")!=null)
+        	if(request.getParameter("pass").length()>=8) {
+        		contraseña=request.getParameter("pass");
+        		for(int i=0; i<contraseña.length();i++) {
+        			if(Character.isDigit(contraseña.charAt(i)))
+        				contraseña+="";
+        		}
+        		
+        	}else
+        		contraseña="La contraserña debe tener mínimo 8 caracteres y al menos una mayuscula y un numero.";
+        else
+        	contraseña="La contraserña debe tener mínimo 8 caracteres y al menos una mayuscula y un numero.";
+
+        out.println("<li>Numero: "+request.getParameter("number")+"</li>");
+
         out.println("<li>Fecha: "+request.getParameter("date")+"</li>");
         
         String[] valoresRadio=request.getParameterValues("radio");
@@ -54,18 +62,9 @@ public class ProcesarDatosServlet extends HttpServlet {
         	out.println("<li>Radio: "+valoresRadio[i]+"</li>");
         
         out.println("<li>Checkbox:</li>");
-
-        out.println("<ul>");
         String[] valoresCheckbox=request.getParameterValues("checkbox");
         for(int i=0;i<valoresCheckbox.length; i++)
         	out.println("<li>"+valoresCheckbox[i]+"</li>");
-        out.println("</ul>");
-        
-        out.println("</ul>");
-       
-        out.println("<a href='./index.html'>Volver</a>");
-        out.close();
-		
 	}
 
 	/**
