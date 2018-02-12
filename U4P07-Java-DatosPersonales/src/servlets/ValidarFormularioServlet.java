c143016d47e6831b80fce4f6880a239da803ce5d
piternoob <pedroplazar94@gmail.com>
2018-02-12 14:16:12 +0100

    .

--- U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
+++ U4P07-Java-DatosPersonales/src/servlets/ValidarFormularioServlet.java
@@ -2,12 +2,16 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import oracle.jrockit.jfr.parser.ParseException;
 
 /**
  * Servlet implementation class ValidarFormularioServlet
@@ -30,41 +34,68 @@ public class ValidarFormularioServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		String nombre, contraseña, mayusculas;
+		String nombre, contraseña, mayusculas="ABCDEFGHIJKLMNÑOPQRSTVWXYZ";
+		boolean mayus=false, num=false;
 		
         PrintWriter out = response.getWriter();
        
-        if(request.getParameter("text")!=null)
+        //nombre
+        if(request.getParameter("text")!=null) {
         	nombre=request.getParameter("text");
-        else
+        	 out.println("<li>Nombre: "+nombre+"</li>");
+        }else{
         	nombre="El campo está vacío.";
-        	
-        out.println("<li>Contraseña: "+request.getParameter("pass")+"</li>");
+        	out.println("<li>Nombre: "+nombre+"</li>");
+        }
+        
+        //pass
         if(request.getParameter("pass")!=null)
         	if(request.getParameter("pass").length()>=8) {
         		contraseña=request.getParameter("pass");
-        		for(int i=0; i<contraseña.length();i++) {
+        		for(int i=0; i<contraseña.length() && (!mayus || !num);i++) {
         			if(Character.isDigit(contraseña.charAt(i)))
-        				contraseña+="";
+        				num=true;
+        			else
+        				for(int j=0; j<mayusculas.length() && !mayus; j++) {
+        					if(mayusculas.charAt(j)==contraseña.charAt(i))
+        						mayus=true;
+        				}
         		}
-        		
+        		if(num && mayus)
+        			contraseña=request.getParameter("pass");
+        		else
+        			contraseña="La contraserña debe tener mínimo 8 caracteres y al menos una mayuscula y un numero.";
         	}else
         		contraseña="La contraserña debe tener mínimo 8 caracteres y al menos una mayuscula y un numero.";
         else
         	contraseña="La contraserña debe tener mínimo 8 caracteres y al menos una mayuscula y un numero.";
 
+        out.println("<li>Contraseña: "+contraseña+"</li>");
+        
+        //numero
         out.println("<li>Numero: "+request.getParameter("number")+"</li>");
 
+        
+        //fecha
+        SimpleDateFormat formatoFechaFormulario=new SimpleDateFormat("yyyy-MM-dd");
+        try {
+        	Date fecha= formatoFechaFormulario.parse(request.getParameter("fecha"));
+        	SimpleDateFormat formatoFechaSalida=new SimpleDateFormat("dd/MM/yyyy");
+        	out.println("<li>Fecha: "+formatoFechaSalida.format(fecha)+"</li>");
+        } catch (Exception e) {
+        	e.printStackTrace();
+		}
         out.println("<li>Fecha: "+request.getParameter("date")+"</li>");
         
         String[] valoresRadio=request.getParameterValues("radio");
         for(int i=0;i<valoresRadio.length; i++)
         	out.println("<li>Radio: "+valoresRadio[i]+"</li>");
         
-        out.println("<li>Checkbox:</li>");
+        out.println("<li>Checkbox:");
         String[] valoresCheckbox=request.getParameterValues("checkbox");
         for(int i=0;i<valoresCheckbox.length; i++)
-        	out.println("<li>"+valoresCheckbox[i]+"</li>");
+        	out.println(valoresCheckbox[i]+", ");
+        out.println("</li>");
 	}
 
 	/**
